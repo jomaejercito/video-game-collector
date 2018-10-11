c31af3938c3362b7bb3d4c8bac14501acebffc4a
Joma Ejercito <jomaejercito@gmail.com>
2018-10-11 18:42:19 -0400

    Added model associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  has_many :collections
+  has_many :users, through: :collections
+  belongs_to :console  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :collections
+  has_many :games, through: :collections
+  has_secure_password  
 end
