8cf94da7df1ee127b5d6944e285077fe88485e25
Joma Ejercito <jomaejercito@gmail.com>
2018-10-11 19:40:11 -0400

    Edited routes for login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
-  get '/login' => 'sessions#new'
-  post '/sessions' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resources :users, only: [:create, :show] do
     resources :collections, except: [:index, :new, :edit]
