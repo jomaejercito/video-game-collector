a6c7499c8cc539cddc00a543edb2c38ea1c158d0
Joma Ejercito <jomaejercito@gmail.com>
2018-10-11 19:25:45 -0400

    Set up login/signup features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if !!user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_path(user)
+    else
+      render :new
+    end
   end
 
+
   def destroy
+    reset_sessions
+    redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
-  get '/login' => 'session#new'
-  post '/sessions' => 'session#create'
-  delete '/logout' => 'session#destroy'
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
 
   resources :users, only: [:create, :show] do
